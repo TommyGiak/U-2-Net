f5c5e1eac4b633af093c0f4b261eb76460717508
Tommaso Giacometti <tommaso.giak@gmail.com>
2025-06-27 08:08:54 +0200

    tom commit

---
+++ u2net_semantic_test.py
@@ -0,0 +1,122 @@
+import os
+from skimage import io, transform
+import torch
+import torchvision
+from torch.autograd import Variable
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.utils.data import Dataset, DataLoader
+from torchvision import transforms#, utils
+# import torch.optim as optim
+
+import numpy as np
+from PIL import Image
+import glob
+
+from data_loader import RescaleT
+from data_loader import ToTensor
+from data_loader import ToTensorLab
+from data_loader import SemanticSalObjDataset
+
+from model import SU2NET # full size version 173.6 MB
+from model import SU2NETP # small version u2net 4.7 MB
+
+
+PATH_TO_WEIGHTS = './saved_models/u2net/u2net_bce_itr_6_train_2.625809_tar_0.579920.pth'
+
+
+# normalize the predicted SOD probability map
+def normPRED(d):
+    semantic_layer = torch.argmax(d, dim=0)
+
+    return (semantic_layer+1)/3.
+
+def save_output(image_name,pred,d_dir):
+
+    predict = pred
+    predict = predict.squeeze()
+    predict_np = predict.cpu().data.numpy()
+
+    im = Image.fromarray(predict_np*255).convert('RGB')
+    img_name = image_name.split(os.sep)[-1]
+    image = io.imread(image_name)
+    imo = im.resize((image.shape[1],image.shape[0]),resample=Image.BILINEAR)
+
+    pb_np = np.array(imo)
+
+    aaa = img_name.split(".")
+    bbb = aaa[0:-1]
+    imidx = bbb[0]
+    for i in range(1,len(bbb)):
+        imidx = imidx + "." + bbb[i]
+
+    imo.save(d_dir+imidx+'.png')
+
+def main(path=PATH_TO_WEIGHTS):
+
+    # --------- 1. get image path and name ---------
+    model_name='u2net'#u2netp
+
+    image_dir = os.path.join(os.getcwd(), 'test_data', 'test_images')
+    prediction_dir = os.path.join(os.getcwd(), 'test_data', model_name + '_results' + os.sep)
+    os.makedirs(prediction_dir, exist_ok=True)
+    model_dir = path
+
+    img_name_list = glob.glob(image_dir + os.sep + '*')
+    print(img_name_list)
+
+    # --------- 2. dataloader ---------
+    #1. dataloader
+    test_salobj_dataset = SemanticSalObjDataset(img_name_list = img_name_list,
+                                        lbl_name_list = [],
+                                        transform=transforms.Compose([RescaleT(320),
+                                                                      ToTensorLab(flag=0)])
+                                        )
+    test_salobj_dataloader = DataLoader(test_salobj_dataset,
+                                        batch_size=1,
+                                        shuffle=False)
+
+    # --------- 3. model define ---------
+    if(model_name=='u2net'):
+        print("...load U2NET")
+        net = SU2NET(3,3)
+    elif(model_name=='u2netp'):
+        print("...load U2NEP")
+        net = SU2NETP(3,3)
+
+    if torch.cuda.is_available():
+        net.load_state_dict(torch.load(model_dir))
+        net.cuda()
+    else:
+        net.load_state_dict(torch.load(model_dir, map_location='cpu'))
+    print(f'Device cuda:{torch.cuda.is_available()}')
+    net.eval()
+
+    # --------- 4. inference for each image ---------
+    for i_test, data_test in enumerate(test_salobj_dataloader):
+
+        print("inferencing:",img_name_list[i_test].split(os.sep)[-1])
+
+        inputs_test = data_test['image']
+        inputs_test = inputs_test.type(torch.FloatTensor)
+
+        if torch.cuda.is_available():
+            inputs_test = Variable(inputs_test.cuda())
+        else:
+            inputs_test = Variable(inputs_test)
+
+        d1,d2,d3,d4,d5,d6,d7= net(inputs_test)
+
+        # normalization
+        pred = F.softmax(torch.squeeze(d1), dim=0) # non sono sicuro e non ho fatto la prova che la dim giusta sia 0, potrebbe dare errore
+        pred = normPRED(pred)
+
+        # save results to test_results folder
+        if not os.path.exists(prediction_dir):
+            os.makedirs(prediction_dir, exist_ok=True)
+        save_output(img_name_list[i_test],pred,prediction_dir)
+
+        del d1,d2,d3,d4,d5,d6,d7
+
+if __name__ == "__main__":
+    main()
